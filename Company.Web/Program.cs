251b41ec26cf7d94d57b21c4218af9863afd4127
salma basem <20191743@fue.edu.eg>
2024-09-09 11:03:07 +0300

    Add project files.

---
+++ Company.Web/Program.cs
@@ -0,0 +1,58 @@
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+using Repository.Repositories;
+using Service.Interfaces;
+using Service.Mapping;
+using Service.Services;
+
+namespace Company.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<CompanyDbContext>( 
+                options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));  
+                });
+
+            // builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+            builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+            builder.Services.AddAutoMapper(X=>X.AddProfile(new EmployeeProfile()));
+            builder.Services.AddAutoMapper(X => X.AddProfile(new DepartmentProfile ()));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) 
+        { }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //   optionsBuilder.UseSqlServer("server=DESKTOP-CPOFDU6\\MSSQLSERVER01; database=CompanyDb; trusted_connections=true;");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //global filtering
+          // modelBuilder.Entity<BaseEntity>().HasQueryFilter(x => !x.IsDeleted);
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<EmployeeViewModel> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
